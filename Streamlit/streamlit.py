429f7c6d186ce40c896f9fe2a0503579067cd276
Eric <karatruc@laposte.net>
2024-12-17 19:14:59 +0100

    images

--- Streamlit/streamlit.py
+++ Streamlit/streamlit.py
@@ -10,72 +10,115 @@ from PIL import Image
 import numpy as np
 
 path = os.path.dirname(os.path.realpath(__file__))
+url_images = 'https://github.com/karatruc/Streamlit/blob/Streamlit/Streamlit/images'
+
+old_catv = ['04','05','06','08','09','11','12','18','19']
+
+catv_moto = ['01','02','30','31','32','33','34','35','36','41','42','43','80','50','60']
+catv_tc = ['37','38','39','40']
+
+var_usag = ['place','catu', 'sexe','age','trajet','secu1','secu2','secu3']
+var_cara = ['mois','jour','lum','agg','int','atm','col']
+var_lieu = ['catr','circ','vosp','prof','plan','surf','infra','situ']
+var_vehi = ['catv','obs', 'obsm','choc','manv','motor']
+
+
+
 
 #chargements des variables
 @st.cache_data
-def variables() :
+def get_variables() :
     variables = load('d:/GIT/Data/libelles_variables.joblib')
     return variables
 
+#chargements des images pour places 
 @st.cache_data
-def get_images() :
-    images = {}
-    images['tc'] = []
-    images['moto'] = []
-    images['car'] = []
+def get_html_places() :
+    cat_vehicules = ['moto','car','tc']
+    cat_vehicule_html = {}
+    for cat in cat_vehicules :
+        with open('{}/images/{}.html'.format(path,cat),'r') as html:
+            cat_vehicule_html[cat] = html.read().replace('{path}',url_images).replace('.jpg','.jpg?raw=true')
+    return cat_vehicule_html
 
-    
+html_places = get_html_places()
 
-    with os.scandir(path + '\images') as files :
-        for file in files :
-            img=file.path
-            
-            if file.name.startswith('tc') :
-                images['tc'].append(img)
-            if file.name.startswith('car') :
-                images['car'].append(img)
-            if file.name.startswith('moto') :
-                images['moto'].append(img)
-         
-    return images
 
-images = get_images()
+vars = get_variables()
 
 
 
-#@st_cache_data
-#def get_html_images() :
+# génère le formulaire : une liste de choix par variable
+options = {}
+
+#caractéristiques
+for k in [x for x in var_cara if x not in ['mois','jour']]:
+    vals = vars[k]['valeurs']
+    options[k] = st.selectbox(key = k, label = vars[k]['variable'], options = list(vals.keys()), format_func = lambda x : vals[x] )
+
+#lieux
+for k in var_lieu :
+    vals = vars[k]['valeurs']
+    options[k] = st.selectbox(key = k, label = vars[k]['variable'], options = list(vals.keys()), format_func = lambda x : vals[x] )
+
+#vehicules
+for k in var_vehi :
+    vals = vars[k]['valeurs']
+    if k == 'catv' :
+        vals = {x:vals[x] for x in vals.keys() if x not in old_catv}
+    options[k] = st.selectbox(key = k, label = vars[k]['variable'], options = list(vals.keys()), format_func = lambda x : vals[x] )
+
+# usagers
+for k in [x for x in var_usag if x not in ['place','age']] :
+    vals = vars[k]['valeurs']
+    options[k] = st.selectbox(key = k, label = vars[k]['variable'], options = list(vals.keys()), format_func = lambda x : vals[x] )
 
 
-#images = get_images()
+st.write(options['catv'])
 
-with open('d:/git/Streamlit/images/tc.html','r') as html:
-    content = html.read().replace('{path}',path)
+
+if options['catv'] in catv_tc :
+    content = html_places['tc']
+elif options['catv'] in catv_moto :
+    content = html_places['moto']
+else  :
+    content = html_places['car']
 
 st.write(content)
+
+
+options['catv'] = click_detector(content)
+
+# for k, v in vars.items() :
+#     if k not in ['place'] :
+#         vals = vars[k]['valeurs']
+        
+#         if k == 'catv' :
+#             vals = vals - old_catv
+
+#         options['k'] = st.selectbox(key = k, label = vars[k]['variable'], options = list(vals.keys()), format_func = lambda x : vals[x] )
+
+
+
+
+
+
+    
+
+
     
 
 
 
-clicked = click_detector(content)
+#clicked = click_detector(content)
 
-st.markdown(f"**{clicked} clicked**" if clicked != "" else "**No click**")
+#st.markdown(f"**{clicked} clicked**" if clicked != "" else "**No click**")
 # #st.markdown(f"Image #{clicked} clicked" if clicked > -1 else "No image clicked")
 
 # st.write(content)
 
 
-# vars = variables()
 
-# def keyValues(key) :
-#     return vars[key]
-
-
-# # génère le formulaire : une liste de choix par variable
-# for k, v in vars.items() :
-#     vals = vars[k]['valeurs']
-    
-#     st.selectbox(key = k, label = vars[k]['variable'], options = list(vals.keys()), format_func = lambda x : vals[x] )
 
 
 
