745a62b4b3626a19fb7839ff8d0bd5c4aeceee26
Eric <eric.favre@gmail.com>
2025-01-02 17:01:45 +0100

    ajout test chi2 + cosmétique

--- Streamlit/streamlit.py
+++ Streamlit/streamlit.py
@@ -1,24 +1,42 @@
+#import des bibliothèques
 import streamlit as st
-from streamlit_folium import st_folium
+#paramètres de la page streamlit
+st.set_page_config(layout="wide")
+st.title('Accidents de la route en France 2019-2023')
+st.markdown(
+    """
+    <style>
+    [data-testid="stTabs"] > div:first-child{
+        width: 1024px;
+    }
+    """,
+    unsafe_allow_html=True,
+)
 
-import folium
-from joblib import load
-import os
-from st_click_detector import click_detector
-#from streamlit_drawable_canvas import st_canvas 
-#from PIL import Image
-import numpy as np
-import datetime
-from catboost import CatBoostClassifier
-import matplotlib.pyplot as plt
-import seaborn as sns
-import sys
+with st.spinner ('Chargement des bibliothèques') :
+
+    from streamlit_folium import st_folium
+    import folium
+    from joblib import load
+    import os
+    from st_click_detector import click_detector
+    import numpy as np
+    import datetime
+    from catboost import CatBoostClassifier
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    import sys
+    from sklearn.cluster import KMeans
+    #from tensorflow.keras.models import Sequential
+    import tensorflow as tf
+    from scipy.stats import chi2_contingency
 
 path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(path)
 
 from Pipeline import *
 
+#listes
 url_images = 'https://github.com/karatruc/Streamlit/blob/Streamlit/Streamlit/images'
 
 old_catv = ['04','05','06','08','09','11','12','18','19']
@@ -35,12 +53,16 @@ var_vehi = ['catv','obs', 'obsm','choc','manv','motor']
 #chargements des variables
 @st.cache_data
 def get_variables() :
+    """ récupères le dictionnaire contenant variables, libellés
+    """
     variables = load('{}/../Data/libelles_variables.joblib'.format(path))
     return variables
 
 #chargements des images pour places 
-@st.cache_data
+@st.cache_data(show_spinner=False)
 def get_html_places() :
+    """ insere les images des places dans les fichiers html
+    """
     cat_vehicules = ['moto','car','tc']
     cat_vehicule_html = {}
     for cat in cat_vehicules :
@@ -49,33 +71,62 @@ def get_html_places() :
     return cat_vehicule_html
 
 #chargements des modèles
-@st.cache_data
+@st.cache_data(show_spinner=False)
 def get_models() :
+    """ Récupère les modèles sauvegardés
+    """
     models = {}
     models['4'] = {}
     models['4']['catboost'] = load('Notebooks/MachineLearning/1_ML_4_classes/2_grid_search_best_model/best_models/catboost_4_classes.gz'.format(path))
     models['4']['histgradientboost'] = load('{}/..//Notebooks/MachineLearning/1_ML_4_classes/2_grid_search_best_model/best_models/HistGradientBoosting_4_classes.gz'.format(path))
+    #models['4']['Réseau de neurones'] = load('{}/../neural_network/results/2class_neural_network_model.joblib'.format(path))
 
     models['2'] = {}
     models['2']['catboost'] = load('{}/..//Notebooks/MachineLearning/2_ML_2_classes/2_grid_search_best_model/best_models/catboost_2_classes.gz'.format(path))
     models['2']['xgboost'] = load('{}/..//Notebooks/MachineLearning/2_ML_2_classes/2_grid_search_best_model/best_models/xgboost_2_classes.gz'.format(path))
+    #models['2']['Réseau de neurones'] = load('{}/../neural_network/results/4class_neural_network_model.joblib'.format(path))
 
     return models
 
 def remove_NR( dico ) :
+    """ Supprime les valeurs -1 du dictionn,aire des variables et valeurs
+    """
     return {i:v for i, v in dico.items() if i != '-1'}
 
-@st.cache_data()
+@st.cache_data(show_spinner=False)
 def get_data() :
+    """ charge les données issues du preprocessing
+    """
     df = pd.read_csv('{}/../Data/accidents.zip'.format(path))
     return df
 
+@st.cache_data(show_spinner=False)
+def get_geoloc_map() :
+    """ Chargement du clustering de geolocalisation
+    """
+    kmeans = load('{}/../Models/clustering_geoloc.joblib'.format(path))
+    centers = kmeans.cluster_centers_
+    labels = range(0,80)
+
+    map = folium.Map(location=[0,0], zoom_start=1)
+
+    for label , center in zip(labels, centers) :
+        folium.Marker(center, 
+                    popup = folium.map.Popup(label, parse_html=True),
+                    tooltip= folium.map.Tooltip(permanent=True, text='<b>{}</b><br/>({:.2f}/{:.2f})'.format(label, center[0], center[1]), sticky=False),
+                    
+                    ).add_to(map)
+    
+    return map
+
 def plot_cat(df, variable, normalize, dico_vars) :
+    """ renvoi un barplot de la gravité en foicntion de la variable fournie
+    """
     palette = ['blue','green', 'orange','red']
     labels = ['Indemne','Blessé léger','Blessé Grave', 'Tué']
     #fig = plt.figure(figsize=(10, 4))
-    fig, ax = plt.subplots()
-
+    fig, ax = plt.subplots(figsize=(12, 5) )
+    
     if normalize :
         df2plot = (df.groupby([variable,'grav'],observed=True).size()*100 / df.groupby(variable, observed=False).size()).reset_index(name='percent')
         sns.barplot(data = df2plot, x=variable, y='percent', hue='grav', palette = palette)
@@ -87,6 +138,7 @@ def plot_cat(df, variable, normalize, dico_vars) :
         plt.ylabel('Nombres d\'usagers par gravité')     
     
     hands, labs = ax.get_legend_handles_labels()
+
     plt.legend(title='Gravité', handles = hands, labels = labels, bbox_to_anchor=(1.35, 1))
     plt.xlabel(dico_vars[variable]['variable'])
     plt.title('Répartition des gravités selon {}'.format(dico_vars[variable]['variable']));
@@ -98,6 +150,9 @@ def plot_cat(df, variable, normalize, dico_vars) :
 
     plt.xticks(ticks = range(len(value_keys)),  labels = [dico_vars[variable]['valeurs'][v] for v in value_keys], rotation = 80);
     st.pyplot(fig)
+    khi, cramer = test_chi2(df,variable,'grav')
+    st.write(khi)
+    st.write(cramer)
 
 def plot_num(df, variable, normalize, dico_vars) :
     palette = ['blue','green', 'orange','red']
@@ -110,31 +165,62 @@ def plot_num(df, variable, normalize, dico_vars) :
     sns.lineplot(data=df2plot,x=variable,y='Count',palette=palette,hue='grav')
     st.pyplot(fig)
 
-st.set_page_config(layout="wide")
+def test_chi2(data_frame , var:str,var_cible:str):
+    
+    ct= pd.crosstab(data_frame[var], data_frame[var_cible])
 
-tabExploration, tabPrevision = st.tabs(["Exploration", "Prévisions"])
+    chi2_stat, p_value, dof, expected_freq =chi2_contingency(ct)
+    
+    #print('Test chi2 (',var,',',var_cible,')','p-value :',p_value)
+
+    result = 'Test Chi² : p =  {}'.format(p_value)
+
+    if p_value<0.05:
+        # Cramer
+        n = ct.sum().sum()
+        min_dim = min(ct.shape) - 1
+        cramer_v = np.sqrt(chi2_stat / (n * min_dim))
+        
+        if cramer_v >= 0.5 :
+            pot = 'forte'
+        elif cramer_v >=0.3 :
+            pot = 'modérée'
+        elif cramer_v >=0.1 :
+            pot = 'faible'
+        else :
+            pot = 'nulle'
+        
+        cramer = 'Score de Cramer-V : {} (Potentielle Correlation {})'.format(cramer_v, pot)
+        
+    return result, cramer
+        # Afficher le coefficient de Cramér-V
+        #print("Potentiel Correlation entre :",var,"et",var_cible,"Coef Cramér-V :", cramer_v)
+        # V = 0 : Aucune association entre les variables.
+        # V proche de 0.1 : Faible association.
+        # V proche de 0.3 : Association modérée.
+        # V proche de 0.5 et plus : Association forte
+
+tabExploration, tabGeolocalisation, tabPrevision = st.tabs(["Exploration","Géolocalisation", "Prévisions"])
 
 with st.spinner('Chargements des données...') :
     html_places = get_html_places()
     vars = get_variables()
     models = get_models()
-
     df_accidents = get_data()
+    map = get_geoloc_map()
 
 with tabExploration :
     variables = list(df_accidents.columns)
-    variables = [v for v in variables if v not in ['lat','long','nbv','vma']]
+    variables = [v for v in variables if v not in ['lat','long','nbv','vma','grav']]
     
     var2plot = st.selectbox(label = 'Variable', options = variables, format_func = lambda x : vars[x]['variable'] )
     norm = st.toggle('Normalisation')
     
     with st.spinner('Tracage du graphique') :
+        plot_cat(df_accidents, var2plot, norm, vars) 
 
-        if var2plot in ['nbv','vma'] :
-            plot_num(df_accidents, var2plot, norm, vars)
-        else :
-            plot_cat(df_accidents, var2plot, norm, vars) 
-
+with tabGeolocalisation :
+    f_map = st_folium(map, use_container_width=True)# width=725)
 
 with tabPrevision :
 
@@ -219,6 +305,8 @@ with tabPrevision :
 
 
     st.subheader("Modèle de prédiction")
+
+
 
     nb_classes = st.selectbox('Nb de classes prédites', list(models.keys()))
 
